dcfbe65fedc3752bd2f175c2a30106ad9cc07864
jdp1ps <joachim.dornbusch@univ-paris1.fr>
2024-11-09 09:15:20 +0100

    Fix Eslint warning

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ import * as path from 'node:path'
 const nextConfig: NextConfig = {
   // for docker https://github.com/vercel/next.js/tree/canary/examples/with-docker
   // output: 'standalone',
-  webpack: (config, { isServer }) => {
+  webpack: (config) => {
     config.resolve.alias['@'] = path.join(__dirname, 'src', 'app')
     return config
   },
