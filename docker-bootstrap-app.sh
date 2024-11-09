4c415121e607bb71a4331b680095abaf6e8b33d7
jdp1ps <joachim.dornbusch@univ-paris1.fr>
2024-11-09 09:34:26 +0100

    Solve dependencies at runtime

--- docker-bootstrap-app.sh
+++ docker-bootstrap-app.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 echo "DATABASE_URL=$DATABASE_URL" > .env
+npm ci --legacy-peer-deps  --include=dev
 npx prisma generate
 DISABLE_ESLINT_PLUGIN=true npm run build
 npx prisma migrate deploy
