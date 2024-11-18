194d21c57ea77815ff7315269229f0e1f9f967ea
KHADHRAOUI bilel <khadhraoui.bilel@devoskills.com>
2024-11-18 15:30:58 +0100

    add error to publication slice

--- src/app/stores/publication_slice.ts
+++ src/app/stores/publication_slice.ts
@@ -3,7 +3,8 @@ import { Publication } from '@prisma/client'
 
 export interface PublicationSlice {
   publications: Publication[]
-  loading: boolean
+  loading: boolean,
+  error: string | null | unknown,
   fetchPublications: () => Promise<void>
 }
 
@@ -15,15 +16,19 @@ export const addPublicationSlice: StateCreator<
 > = (set) => ({
   publications: [],
   loading: true,
+  error: null,
   fetchPublications: async () => {
     set({ loading: true })
     try {
       const response = await fetch('/api/publications') // Replace with your API endpoint
       const jsonData: Publication[] = await response.json()
-      set({ publications: jsonData, loading: false })
+      set({ publications: jsonData})
     } catch (error) {
       console.error('Failed to fetch publications', error)
+      set({ error })
+    } finally {
       set({ loading: false })
     }
+
   },
 })
