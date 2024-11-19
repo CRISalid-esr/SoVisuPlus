ebe0cd0354dc2c70646c97900241e166de5f87cf
KHADHRAOUI bilel <khadhraoui.bilel@devoskills.com>
2024-11-19 03:06:49 +0100

    add unit test for route publications

--- src/app/[lang]/api/publications/route.ts
+++ src/app/[lang]/api/publications/route.ts
@@ -1,13 +1,13 @@
-import { NextResponse } from 'next/server'
-import { PrismaClient, Publication } from '@prisma/client'
+import { NextResponse } from 'next/server';
+import prisma from '@/lib/prisma';
+import { Publication } from '@prisma/client'
 
 export const GET = async () => {
-  const prisma = new PrismaClient()
   try {
     const publications: Publication[] = await prisma.publication.findMany()
-    return NextResponse.json(publications)
+    return NextResponse.json(publications);
   } catch (error) {
-    console.error(error)
-    return NextResponse.json({ error: 'An error occurred' }, { status: 500 })
+    console.error('Error fetching publications:', error);
+    return NextResponse.json({ error: 'An error occurred' }, { status: 500 });
   }
-}
+};
