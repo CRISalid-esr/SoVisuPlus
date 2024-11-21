b151b73aa93296beb47306c628badd35217fa057
bilelkhadhraoui <161612704+bilelkhadhraoui@users.noreply.github.com>
2024-11-21 17:36:12 +0100

    fix import

--- src/app/[lang]/dashboard/layout.tsx
+++ src/app/[lang]/dashboard/layout.tsx
@@ -1,4 +1,4 @@
-import Sidebar from '@/app/[lang]/components/sidebar/sidebar';
+import { Sidebar } from '@/components/sidebar';
 
 export default function DashboardLayout({ children }: { children: React.ReactNode }) {
   return (
