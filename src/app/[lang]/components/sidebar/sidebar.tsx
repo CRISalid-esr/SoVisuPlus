1b0226df7063a8849d33ab68865544e5731f1330
bilelkhadhraoui <161612704+bilelkhadhraoui@users.noreply.github.com>
2024-11-21 19:57:35 +0100

    add all pages

--- src/app/[lang]/components/sidebar/sidebar.tsx
+++ src/app/[lang]/components/sidebar/sidebar.tsx
@@ -4,15 +4,23 @@ import { Trans } from '@lingui/macro'
 import AccountCircleIcon from '@mui/icons-material/AccountCircle'
 import HomeIcon from '@mui/icons-material/Home'
 import MenuIcon from '@mui/icons-material/Menu'
-import { Drawer, IconButton, List, ListItem, ListItemIcon, ListItemText } from '@mui/material'
+import {
+  Drawer,
+  IconButton,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+} from '@mui/material'
+import { Box } from '@mui/system'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 import { useState } from 'react'
 
 export default function Sidebar() {
   const [open, setOpen] = useState(true) // Determines if the drawer is expanded or collapsed
-  const pathname = usePathname(); // Get the current path
-  const lang = pathname.split('/')[1]; // Extract the `lang` dynamic segment
+  const pathname = usePathname() // Get the current path
+  const lang = pathname.split('/')[1] // Extract the `lang` dynamic segment
   const handleToggleDrawer = () => {
     setOpen((prev) => !prev)
   }
@@ -30,9 +38,8 @@ export default function Sidebar() {
       >
         <MenuIcon />
       </IconButton>
-
       <Drawer
-        variant="permanent"
+        variant='permanent'
         open={open}
         sx={{
           '& .MuiDrawer-paper': {
@@ -42,22 +49,81 @@ export default function Sidebar() {
           },
         }}
       >
-        <List>
-          {/* Home Link */}
-          <ListItem  component={Link} href={`/${lang}/dashboard`}>
-            <ListItemIcon>
-              <HomeIcon />
-            </ListItemIcon>
-            {open && <ListItemText primary={<Trans>Home</Trans>} />}
-          </ListItem>
-          {/* My Account Link */}
-          <ListItem  component={Link} href={`/${lang}/dashboard/my-account`}>
+        <Box>
+          <List>
+            <ListItem component={Link} href={`/${lang}/dashboard`}>
+              <ListItemIcon>
+                <HomeIcon />
+              </ListItemIcon>
+              {open && (
+                <ListItemText primary={<Trans>side_bar_dashboard</Trans>} />
+              )}
+            </ListItem>
+            <ListItem component={Link} href={`/${lang}/dashboard/publications`}>
+              <ListItemIcon>
+                <AccountCircleIcon />
+              </ListItemIcon>
+              {open && (
+                <ListItemText primary={<Trans>side_bar_publications</Trans>} />
+              )}
+            </ListItem>
+            <ListItem component={Link} href={`/${lang}/dashboard/expertise`}>
+              <ListItemIcon>
+                <AccountCircleIcon />
+              </ListItemIcon>
+              {open && (
+                <ListItemText primary={<Trans>side_bar_expertise</Trans>} />
+              )}
+            </ListItem>
+            <ListItem
+              component={Link}
+              href={`/${lang}/dashboard/research-activities`}
+            >
+              <ListItemIcon>
+                <AccountCircleIcon />
+              </ListItemIcon>
+              {open && (
+                <ListItemText
+                  primary={<Trans>side_bar_research_activities</Trans>}
+                />
+              )}
+            </ListItem>
+          </List>
+        </Box>
+        <Box>
+          <ListItem component={Link} href={`/${lang}/dashboard/my-groups`}>
             <ListItemIcon>
               <AccountCircleIcon />
             </ListItemIcon>
-            {open && <ListItemText primary={<Trans>My Account</Trans>} />}
+            {open && (
+              <ListItemText primary={<Trans>side_bar_my-groups</Trans>} />
+            )}
           </ListItem>
-        </List>
+          <ListItem component={Link} href={`/${lang}/dashboard/my-groups`}>
+            <ListItemIcon>
+              <AccountCircleIcon />
+            </ListItemIcon>
+            {open && (
+              <ListItemText primary={<Trans>side_bar_my_groups</Trans>} />
+            )}
+          </ListItem>
+          <ListItem component={Link} href={`/${lang}/dashboard/institutions`}>
+            <ListItemIcon>
+              <AccountCircleIcon />
+            </ListItemIcon>
+            {open && (
+              <ListItemText primary={<Trans>side_bar_institutions</Trans>} />
+            )}
+          </ListItem>
+          <ListItem component={Link} href={`/${lang}/dashboard/laboratories`}>
+            <ListItemIcon>
+              <AccountCircleIcon />
+            </ListItemIcon>
+            {open && (
+              <ListItemText primary={<Trans>side_bar_laboratories</Trans>} />
+            )}
+          </ListItem>
+        </Box>
       </Drawer>
     </>
   )
