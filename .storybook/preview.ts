92696a6597da11e529073a1bdb343095b88eeefd
KHADHRAOUI bilel <khadhraoui.bilel@devoskills.com>
2024-11-29 18:05:58 +0100

    fix build

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,5 +1,19 @@
 import type { Preview } from '@storybook/react'
 
+import { ThemeProvider, CssBaseline } from '@mui/material'
+import { withThemeFromJSXProvider } from '@storybook/addon-themes'
+
+/* TODO: update import for your custom Material UI themes */
+import { lightTheme, darkTheme } from '../src/app/theme/theme'
+
+// .storybook/preview.js
+
+import '@fontsource/roboto/300.css'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/500.css'
+import '@fontsource/roboto/700.css'
+import '@fontsource/material-icons'
+
 const preview: Preview = {
   parameters: {
     controls: {
@@ -9,6 +23,19 @@ const preview: Preview = {
       },
     },
   },
+
+  decorators: [
+    withThemeFromJSXProvider({
+      GlobalStyles: CssBaseline,
+      Provider: ThemeProvider,
+      themes: {
+        // Provide your custom themes here
+        light: lightTheme,
+        dark: darkTheme,
+      },
+      defaultTheme: 'light',
+    }),
+  ],
 }
 
 export default preview
