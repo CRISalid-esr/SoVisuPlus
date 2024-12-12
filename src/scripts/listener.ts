985bf45c0f2a6371985ebf43f969c9a6b5068595
jdp1ps <joachim.dornbusch@univ-paris1.fr>
2024-12-12 06:51:11 +0100

    Connect app to Rabbitmq

---
+++ src/scripts/listener.ts
@@ -0,0 +1,17 @@
+import * as dotenv from 'dotenv'
+import AmqpConnection from '../app/lib/amqp/amqpConnection'
+
+dotenv.config()
+;(async () => {
+  try {
+    console.log('Connecting to RabbitMQ...')
+    const connection = new AmqpConnection()
+    await connection.connect()
+    console.log('Connected to RabbitMQ')
+    await connection.consume((msg) => {
+      console.log('Received message from RabbitMQ:', msg)
+    })
+  } catch (error) {
+    console.error('Error connecting to RabbitMQ:', error)
+  }
+})()
