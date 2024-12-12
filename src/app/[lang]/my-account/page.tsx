de3b5ac29488b4f2973233df21a7d519947a7c86
KHADHRAOUI bilel <khadhraoui.bilel@devoskills.com>
2024-12-12 17:06:55 +0100

    fix lint

--- src/app/[lang]/my-account/page.tsx
+++ src/app/[lang]/my-account/page.tsx
@@ -5,12 +5,10 @@ import { Box, Typography } from '@mui/material'
 
 export default function MyAccountPage() {
   return (
-    <>
-      <Box sx={{ padding: 4 }}>
-        <Typography variant='h4' gutterBottom>
-          <Trans>side_bar_my_account</Trans>
-        </Typography>
-      </Box>
-    </>
+    <Box sx={{ padding: 4 }}>
+      <Typography variant='h4' gutterBottom>
+        <Trans>side_bar_my_account</Trans>
+      </Typography>
+    </Box>
   )
 }
