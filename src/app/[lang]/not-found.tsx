20e1ff2516b9111a4814101477e569dac65fcb28
KHADHRAOUI bilel <khadhraoui.bilel@devoskills.com>
2024-12-26 14:35:47 +0100

    add 404 page

--- src/app/[lang]/not-found.tsx
+++ src/app/[lang]/not-found.tsx
@@ -1,13 +1,13 @@
 'use client'
 
-import { Box, Button, Typography } from '@mui/material'
-import { t } from '@lingui/macro'
-import Logo from '@/public/icons/logo_splash_screen.svg'
-import Avatars from '@/public/icons/avatars.svg'
-import { signIn } from 'next-auth/react'
-import Background from '@/public/icons/background.svg'
-import ErrorFallback from '@/components/ErrorFallback'
 import ErrorBoundary from '@/components/ErrorBoundary'
+import ErrorFallback from '@/components/ErrorFallback'
+import Background from '@/public/icons/background.svg'
+import Error404 from '@/public/icons/error404.svg'
+import Logo from '@/public/icons/logo_splash_screen.svg'
+import { Trans } from '@lingui/macro'
+import { Box, Button, Typography } from '@mui/material'
+import { signIn } from 'next-auth/react'
 
 const NotFound = () => {
   return (
@@ -33,9 +33,18 @@ const NotFound = () => {
                   '&:hover': { backgroundColor: 'darkcyan' },
                 }}
               >
-                {t`splash.login`}
+                <Trans>notfound_page_back_home</Trans>
               </Button>
             </Box>
+          </Box>
+          <Box
+            sx={{
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}
+          >
+            <Error404 />
           </Box>
         </Box>
         <Box
@@ -59,52 +68,49 @@ const NotFound = () => {
               zIndex: -1,
             }}
           />
-          <Typography
-            variant='displayLarge'
-            sx={(theme) => ({
-              mb: theme.spacing(18),
-              zIndex: 1,
-            })}
-          >
-            Lorem Ipsum
-          </Typography>
           <Box
             sx={{
-              maxWidth: '478.543px',
-              position: 'relative',
+              display: 'flex',
+              alignItems: 'start',
+              flexDirection: 'column',
             }}
           >
             <Typography
-              component={'p'}
-              variant='headingSmall'
+              variant='displayLarge'
               sx={(theme) => ({
                 mb: theme.spacing(18),
                 zIndex: 1,
+                textAlign: 'start',
               })}
             >
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              404
             </Typography>
-          </Box>
-          <Box
-            sx={{
-              position: 'relative',
-              zIndex: 1,
-              display: 'flex',
-              justifyContent: 'space-between',
-              alignItems: 'center',
-            }}
-          >
-            <Avatars />
-            <Typography
-              variant='bodyLarge'
-              component={'p'}
+            <Box
               sx={{
-                lineHeight: 'normal',
+                maxWidth: '478.543px',
+                position: 'relative',
               }}
             >
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit
-            </Typography>
+              <Typography
+                component={'p'}
+                variant='headingSmall'
+                sx={(theme) => ({
+                  mb: theme.spacing(18),
+                  zIndex: 1,
+                })}
+              >
+                <Trans>notfound_page_description</Trans>
+              </Typography>
+            </Box>
+            <Box
+              sx={{
+                position: 'relative',
+                zIndex: 1,
+                display: 'flex',
+                justifyContent: 'space-between',
+                alignItems: 'center',
+              }}
+            ></Box>
           </Box>
         </Box>
       </Box>
