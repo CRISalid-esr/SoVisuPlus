f77b605b3563f47889373336bd1373d17cca79e7
jdp1ps <joachim.dornbusch@univ-paris1.fr>
2025-01-10 18:33:22 +0100

    Start docker with listener

--- docker-bootstrap-app.sh
+++ docker-bootstrap-app.sh
@@ -24,4 +24,4 @@ npx prisma migrate deploy
 # npm warn exec The following package was not found and will be installed: prisma@5.22.0
 rm -rf node_modules/prisma
 
-HOSTNAME="0.0.0.0" node server.js
+npm run start:all
