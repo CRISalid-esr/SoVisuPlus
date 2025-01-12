b4cc882bfd8deba0abd512b72bf542d79c3c6d18
jdp1ps <joachim.dornbusch@univ-paris1.fr>
2025-01-12 16:07:55 +0100

    Fix integration tests

--- jest.integration.setup.ts
+++ jest.integration.setup.ts
@@ -1,14 +1,37 @@
+import prisma from '@/lib/daos/prisma'
 import '@testing-library/jest-dom'
 
 import { execSync } from 'child_process'
 
 process.env.DATABASE_URL = process.env.DATABASE_URL_TEST
 
-beforeEach(() => {
+beforeAll(() => {
   execSync('npx prisma db push --accept-data-loss', {
     env: {
       ...process.env,
       DATABASE_URL: process.env.DATABASE_URL_TEST,
     } as NodeJS.ProcessEnv,
   })
+})
+
+const clearDatabase = async () => {
+  console.log('****Clearing database...')
+  await prisma.$executeRawUnsafe(`
+    TRUNCATE TABLE 
+      "PersonIdentifier", 
+      "User", 
+      "Person",
+      "ResearchStructure",
+      "ResearchStructureIdentifier"
+    RESTART IDENTITY CASCADE;
+  `)
+  console.log('****Database cleared')
+}
+
+beforeEach(async () => {
+  await clearDatabase()
+})
+
+afterAll(async () => {
+  await prisma.$disconnect()
 })
--- tests/prisma/clearDatabase.ts
+++
@@ -1,9 +0,0 @@
-import { PrismaClient } from '@prisma/client'
-
-export async function clearDatabase(prisma: PrismaClient) {
-  await prisma.$transaction([
-    prisma.personIdentifier.deleteMany(),
-    prisma.user.deleteMany(),
-    prisma.person.deleteMany(),
-  ])
-}
