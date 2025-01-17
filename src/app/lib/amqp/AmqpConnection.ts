5db38d56f6174397e1a659bba0bcead7acd10cc9
jdp1ps <joachim.dornbusch@univ-paris1.fr>
2025-01-17 11:25:46 +0100

    Add automatic binding

--- src/app/lib/amqp/AmqpConnection.ts
+++ src/app/lib/amqp/AmqpConnection.ts
@@ -4,9 +4,16 @@ import { Channel, Connection } from 'amqplib'
 type AMQPMessageHandler = (message: string) => void
 
 class AmqpConnection {
-  connection!: Connection
-  channel!: Channel
-  private connected!: boolean
+  private connection!: Connection
+  private channel!: Channel
+  private connected: boolean
+  private readonly DEFAULT_QUEUE_NAME = 'sovisuplus'
+  private readonly EXCHANGE_NAME = 'amqp.topic'
+  private readonly BINDING_KEYS = [
+    'event.people.person.*',
+    'event.structures.structure.*',
+    'event.documents.document.*',
+  ]
 
   constructor() {
     this.connected = false
@@ -14,12 +21,18 @@ class AmqpConnection {
 
   async connect() {
     if (this.connected && this.channel) return
+
     try {
       this.connection = await client.connect(
         `amqp://${process.env.AMQP_USER}:${process.env.AMQP_PASSWORD}@${process.env.AMQP_HOST}:${process.env.AMQP_PORT}`,
       )
       this.channel = await this.connection.createChannel()
+
       await this.channel.prefetch(1)
+
+      await this.channel.assertExchange(this.EXCHANGE_NAME, 'topic', {
+        durable: true,
+      })
       this.connected = true
     } catch (error) {
       console.error(error)
@@ -27,26 +40,25 @@ class AmqpConnection {
     }
   }
 
-  private readonly DEFAULT_QUEUE_NAME = 'sovisuplus'
-
   async consume(handleIncomingNotification: AMQPMessageHandler) {
-    await this.channel.assertQueue(
-      process.env.AMQP_QUEUE_NAME || this.DEFAULT_QUEUE_NAME,
-      {
-        durable: true,
-      },
-    )
+    const queueName = process.env.AMQP_QUEUE_NAME || this.DEFAULT_QUEUE_NAME
+
+    await this.channel.assertQueue(queueName, {
+      durable: true,
+    })
+
+    for (const bindingKey of this.BINDING_KEYS) {
+      await this.channel.bindQueue(queueName, this.EXCHANGE_NAME, bindingKey)
+    }
 
     await this.channel.consume(
-      process.env.AMQP_QUEUE_NAME || 'sovisuplus',
+      queueName,
       (message) => {
-        {
-          if (!message) {
-            return console.error(`Invalid incoming message`)
-          }
-          handleIncomingNotification(message?.content?.toString())
-          this.channel.ack(message)
+        if (!message) {
+          return console.error(`Invalid incoming message`)
         }
+        handleIncomingNotification(message.content.toString())
+        this.channel.ack(message)
       },
       {
         noAck: false,
