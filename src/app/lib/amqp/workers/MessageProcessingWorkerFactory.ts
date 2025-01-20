bbcca84fffffe4b3a6583e4d0cc4c9e2bca799c2
KHADHRAOUI bilel <khadhraoui.bilel@devoskills.com>
2025-01-20 11:16:26 +0100

    add document worker

--- src/app/lib/amqp/workers/MessageProcessingWorkerFactory.ts
+++ src/app/lib/amqp/workers/MessageProcessingWorkerFactory.ts
@@ -3,12 +3,15 @@ import { AMQPEntityData } from '@/types/AMQPEntityData'
 import { PersonWorker } from '@/lib/amqp/workers/PersonWorker'
 import { MessageProcessingWorker } from '@/lib/amqp/workers/MessageProcessingWorker'
 import { ResearchStructureWorker } from '@/lib/amqp/workers/ResearchStructureWorker'
+import { DocumentWorker } from '@/lib/amqp/workers/DocumentWorker'
 import { AMQPPersonMessage } from '@/types/AMQPPersonMessage'
 import { AMQPResearchStructureMessage } from '@/types/AMQPResearchStructureMessage'
 import { ResearchStructureDAO } from '@/lib/daos/ResearchStructureDAO'
+import { DocumentDAO } from '@/lib/daos/DocumentDAO'
 import { PersonDAO } from '@/lib/daos/PersonDAO'
 import { UserDAO } from '@/lib/daos/UserDAO'
 import { PersonGraphQLClient } from '@/lib/graphql/PersonGraphQLClient'
+import { AMQPDocumentMessage } from '@/types/AMQPDocumentMessage'
 
 export class MessageProcessingWorkerFactory {
   createWorker(
@@ -26,6 +29,11 @@ export class MessageProcessingWorkerFactory {
         return new ResearchStructureWorker(
           message as AMQPResearchStructureMessage,
           new ResearchStructureDAO(),
+        )
+      case 'document':
+        return new DocumentWorker(
+          message as AMQPDocumentMessage,
+          new DocumentDAO(),
         )
       default:
         throw new Error(`Unsupported message type: ${message.type}`)
