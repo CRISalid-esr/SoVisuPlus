1052a0bea8a30050565264ed820bdf247b8c0853
KHADHRAOUI bilel <khadhraoui.bilel@devoskills.com>
2025-01-20 11:51:55 +0100

    add document query

---
+++ src/app/lib/graphql/DocumentGraphQLClient.ts
@@ -0,0 +1,41 @@
+import { AbstractGraphQLClient } from './AbstractGraphQLClient'
+import { Document } from '@/types/Document'
+import { loadQuery } from '@/lib/graphql/queries/loadQuery'
+
+interface GraphDocumentResponse {
+  uid: string
+  titles: Record<string, string>
+}
+
+export interface GraphDocumentsResponse {
+  documents: Array<GraphDocumentResponse>
+}
+
+export class DocumentGraphQLClient extends AbstractGraphQLClient {
+  /**
+   * Get a document by their UID
+   * @param uid
+   * @returns The document if found, null otherwise
+   */
+  public async getDocumentByUid(uid: string): Promise<Document | null> {
+    const variables = {
+      where: {
+        uid_EQ: uid,
+      },
+    }
+    const documentQuery = loadQuery('document.graphql')
+
+    const response: GraphDocumentsResponse =
+      await this.query<GraphDocumentsResponse>(documentQuery, variables)
+    const [documentData] = response.documents
+
+    if (!documentData) {
+      return null
+    }
+    return this.hydrate(documentData)
+  }
+
+  private hydrate(personData: GraphDocumentResponse): Document {
+    return new Document(personData.uid, personData.titles)
+  }
+}
