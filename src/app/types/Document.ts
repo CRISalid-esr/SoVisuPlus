f632f0b747cd37a5acc3743da3b63f7500e5f01c
KHADHRAOUI bilel <khadhraoui.bilel@devoskills.com>
2025-01-20 15:50:50 +0100

    has_contributions

--- src/app/types/Document.ts
+++ src/app/types/Document.ts
@@ -1,7 +1,10 @@
+import { Person } from './Person'
+
 class Document {
   constructor(
     public uid: string,
     public titles: Record<string, string>,
+    public has_contributions: Array<Person> = [],
   ) {}
 }
 
