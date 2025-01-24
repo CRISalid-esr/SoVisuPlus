9236198acc33c27ad4c775c9ed2e723853461030
KHADHRAOUI bilel <khadhraoui.bilel@devoskills.com>
2025-01-24 16:25:22 +0100

    add tabs to my account

--- src/app/[lang]/my-account/page.tsx
+++ src/app/[lang]/my-account/page.tsx
@@ -1,14 +1,63 @@
 'use client'
 
-import { Trans } from '@lingui/macro'
+import { TabFilter } from '@/components/TabFilter'
+import { t, Trans } from '@lingui/macro'
 import { Box, Typography } from '@mui/material'
+import { useTheme } from '@mui/system'
+import { useState } from 'react'
 
 export default function MyAccountPage() {
+  const theme = useTheme()
+
+  const tabs = [
+    {
+      label: t`my_account_page_my_profile_tab`,
+      value: 'my_profile',
+    },
+    {
+      label: t`my_account_page_authorizations_tab`,
+      value: 'authorizations',
+    },
+    {
+      label: t`my_account_page_notification_tab`,
+      value: 'notification',
+    },
+  ]
+
+  const [selectedTab, setSelectedTab] = useState(tabs[0].value)
+
+  const handleTabeChange = (newValue: string) => {
+    setSelectedTab(newValue)
+  }
+
   return (
-    <Box sx={{ padding: 4 }}>
-      <Typography variant='h4' gutterBottom>
-        <Trans>side_bar_my_account</Trans>
-      </Typography>
+    <Box>
+      <Box
+        mb={3}
+        sx={{
+          display: 'flex',
+          justifyContent: 'space-between',
+          alignItems: {
+            xs: 'flex-start',
+            md: 'center',
+          },
+          flexDirection: {
+            xs: 'column',
+            sm: 'row',
+          },
+        }}
+      >
+        <Box>
+          <Typography variant='h4' gutterBottom>
+            <Trans>side_bar_my_account</Trans>
+          </Typography>
+        </Box>
+      </Box>
+      <TabFilter
+        tabsData={tabs}
+        selectedValue={selectedTab}
+        onTabChange={handleTabeChange}
+      />
     </Box>
   )
 }
