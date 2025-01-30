a6f0f5d7130727f3c0f4b6392d93f8525fc381e9
KHADHRAOUI bilel <khadhraoui.bilel@devoskills.com>
2025-01-30 13:00:24 +0100

    refacto document service document doa

--- src/app/api/documents/route.ts
+++ src/app/api/documents/route.ts
@@ -1,6 +1,5 @@
-import prisma from '@/lib/prisma'
-import { Prisma } from '@prisma/client'
 import { NextRequest, NextResponse } from 'next/server'
+import { DocumentService } from '@/lib/services/DocumentService'
 
 export const GET = async (req: NextRequest) => {
   try {
@@ -12,152 +11,19 @@ export const GET = async (req: NextRequest) => {
     const columnFilters = JSON.parse(urlParams.get('columnFilters') || '[]')
     const sorting = JSON.parse(urlParams.get('sorting') || '[]')
 
-    const skip = (page - 1) * pageSize
-
-    // Define language priority order
-    const languagePriority = ['fr', 'en', 'es', 'it'] // Default languages
-    const langOrder = [
+    const documentService = new DocumentService()
+    const { documents, totalItems } = await documentService.fetchDocuments({
+      searchTerm,
+      page,
+      pageSize,
       lang,
-      ...languagePriority.filter((language) => language !== lang),
-    ]
-
-    let query = Prisma.sql`
-      SELECT d.id,
-        d.uid,
-        p."firstName" AS contributor_firstName, p."lastName" AS contributor_lastName,
-        p."id" AS contributor_id, p."uid" AS contributor_uid, p."email" AS contributor_email,
-        c."role" as role, c."id" as contribution_id,
-        t.value AS title_value, t.language AS title_language, t.id AS title_id
-      FROM "Document" d
-      LEFT JOIN "DocumentTitle" t ON t."documentId" = d.id
-      LEFT JOIN "Contribution" c ON c."documentId" = d.id
-      LEFT JOIN "Person" p ON p.id = c."personId"
-      WHERE 1 = 1
-    `
-
-    // Apply searchTerm filter (dynamically handle fallback languages)
-    if (searchTerm) {
-      // Construct the search condition for each language in langOrder
-      const languageSearchConditions = langOrder.map((language) => {
-        return Prisma.sql`
-          (t.language = ${language} AND t.value ILIKE ${`%${searchTerm}%`})
-        `
-      })
-
-      // Combine all language search conditions with OR
-      query = Prisma.sql`
-        ${query} AND (
-          ${Prisma.join(languageSearchConditions, ' OR ')}
-          OR (p."firstName" ILIKE ${`%${searchTerm}%`} OR p."lastName" ILIKE ${`%${searchTerm}%`})
-        )
-      `
-    }
-
-    // Apply column filters (with title filtering also considering fallback languages)
-    columnFilters.forEach((filter: { id: string; value: string }) => {
-      if (filter.id === 'titles') {
-        query = Prisma.sql`
-          ${query} AND (
-            ${Prisma.join(
-              langOrder.map(
-                (language) => Prisma.sql`
-                (t.language = ${language} AND t.value ILIKE ${`%${filter.value}%`})
-              `,
-              ),
-              ' OR ',
-            )}
-          )
-        `
-      } else if (filter.id === 'contributions') {
-        query = Prisma.sql`
-          ${query} AND (p."firstName" ILIKE ${`%${filter.value}%`} OR p."lastName" ILIKE ${`%${filter.value}%`})
-        `
-      }
+      columnFilters,
+      sorting,
     })
-
-    let orderQuery = Prisma.empty
-    sorting.forEach((sort: { id: string; desc: boolean }) => {
-      if (sort.id === 'contributions') {
-        orderQuery = Prisma.sql`
-          ORDER BY 
-            NULLIF(contributor_firstName, '') IS NULL ASC, contributor_firstName ${sort.desc ? Prisma.raw('DESC') : Prisma.raw('ASC')} NULLS LAST, 
-            NULLIF(contributor_lastName, '') IS NULL ASC, contributor_lastName ${sort.desc ? Prisma.raw('DESC') : Prisma.raw('ASC')} NULLS LAST
-        `
-      } else if (sort.id === 'titles') {
-        const orderClauses = langOrder.map((language) => {
-          return Prisma.raw(`
-            NULLIF(title_value, '') IS NULL ASC,
-            title_language = '${language}' DESC,
-            title_language != '${language}' ASC
-          `)
-        })
-        orderQuery = Prisma.sql`
-          ORDER BY 
-            ${Prisma.join(orderClauses, ', ')}, 
-            title_value ${sort.desc ? Prisma.raw('DESC') : Prisma.raw('ASC')} NULLS LAST
-        `
-      }
-    })
-
-    query = Prisma.sql`
-      WITH grouped_documents AS (
-        ${query}
-      )
-      SELECT
-        id, 
-        uid,
-        ARRAY_AGG(DISTINCT JSONB_BUILD_OBJECT(
-          'id', title_id, 'language', title_language, 'value', title_value
-        )) AS titles,
-        ARRAY_AGG(DISTINCT JSONB_BUILD_OBJECT(
-          'id', contribution_id, 'role', role, 'person', JSONB_BUILD_OBJECT(
-            'id', contributor_id, 'firstName', contributor_firstName,
-            'lastName', contributor_lastName, 'uid', contributor_uid, 'email', contributor_email
-          )
-        )) AS contributions
-      FROM grouped_documents
-      GROUP BY id, uid ,title_value ,contributor_firstName, contributor_lastName,title_language
-      ${orderQuery}
-      OFFSET ${skip} LIMIT ${pageSize}
-    `
-
-    console.log('=>', query.sql)
-    console.log('=>', query.values)
-
-    const documents: any[] = await prisma.$queryRaw(query).catch((error) => {
-      console.error(error)
-      return []
-    })
-
-    const countQuery = Prisma.sql`
-      SELECT COUNT(DISTINCT d.id) AS total
-      FROM "Document" d
-      LEFT JOIN "DocumentTitle" t ON t."documentId" = d.id
-      LEFT JOIN "Contribution" c ON c."documentId" = d.id
-      LEFT JOIN "Person" p ON p.id = c."personId"
-      WHERE 1 = 1
-    `
-
-    const languageSearchConditions = langOrder.map((language) => {
-      return Prisma.sql`
-        (t.language = ${language} AND t.value ILIKE ${`%${searchTerm}%`})
-      `
-    })
-
-    const totalCount = await prisma.$queryRaw(
-      searchTerm
-        ? Prisma.sql`${countQuery} AND (
-            ${Prisma.join(languageSearchConditions, ' OR ')}
-            OR (p."firstName" ILIKE ${`%${searchTerm}%`} OR p."lastName" ILIKE ${`%${searchTerm}%`})
-          )`
-        : countQuery,
-    )
-
-    console.log('totalCount[0].total:', parseInt(totalCount[0].total, 10))
 
     return NextResponse.json({
-      documents: documents,
-      totalItems: parseInt(totalCount[0].total, 10),
+      documents,
+      totalItems,
       page,
       limit: pageSize,
     })
---
+++ src/app/lib/services/DocumentService.ts
@@ -0,0 +1,42 @@
+import { DocumentDAO } from '@/lib/daos/DocumentDAO'
+
+interface FetchDocumentsParams {
+  searchTerm: string
+  page: number
+  pageSize: number
+  lang: string
+  columnFilters: any[]
+  sorting: any[]
+}
+
+export class DocumentService {
+  private documentDAO: DocumentDAO
+  constructor() {
+    this.documentDAO = new DocumentDAO() // Instantiate the DAO class
+  }
+
+  async fetchDocuments({
+    searchTerm,
+    page,
+    pageSize,
+    lang,
+    columnFilters,
+    sorting,
+  }: FetchDocumentsParams) {
+    try {
+      const { documents, totalItems } =
+        await this.documentDAO.fetchDocumentsFromDB({
+          searchTerm,
+          page,
+          pageSize,
+          lang,
+          columnFilters,
+          sorting,
+        })
+      return { documents, totalItems }
+    } catch (error) {
+      console.error('Error in service layer:', error)
+      throw new Error('Error fetching documents from service')
+    }
+  }
+}
