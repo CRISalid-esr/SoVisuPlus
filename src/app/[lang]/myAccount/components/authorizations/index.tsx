2297c35612ff5ec8f4376e6758f56453978b67fc
KHADHRAOUI bilel <khadhraoui.bilel@devoskills.com>
2025-02-07 16:04:49 +0100

    add authorizations page

--- src/app/[lang]/myAccount/components/authorizations/index.tsx
+++ src/app/[lang]/myAccount/components/authorizations/index.tsx
@@ -1,25 +1,64 @@
-import Button from '@/app/theme/overrides/Button'
-import { CardActions, CardContent, Typography } from '@mui/material'
+import { CustomCard } from '@/components/Card'
+import { Trans } from '@lingui/react'
+import {
+  CardContent,
+  Checkbox,
+  FormControlLabel,
+  Typography,
+} from '@mui/material'
+import { useTheme } from '@mui/material/styles'
+import { Box } from '@mui/system'
 
 export default function AuthorizationsPage() {
+  const theme = useTheme()
+
   return (
-    <>
-      <CardContent>
-        <Typography gutterBottom sx={{ color: 'text.secondary', fontSize: 14 }}>
-          Word of the Day
-        </Typography>
-        <Typography variant='h5' component='div'>
-          lorem
-        </Typography>
-        <Typography sx={{ color: 'text.secondary', mb: 1.5 }}>
-          adjective
-        </Typography>
-        <Typography variant='body2'>
-          well meaning and kindly.
-          <br />
-          {'"a benevolent smile"'}
-        </Typography>
-      </CardContent>
-    </>
+    <Box>
+      <CustomCard
+        header={
+          <Typography
+            sx={{
+              color: theme.palette.primary.main,
+              fontSize: theme.utils.pxToRem(20),
+              fontStyle: 'normal',
+              fontWeight: theme.typography.fontWeightRegular,
+              lineHeight: 'normal',
+            }}
+          >
+            <Trans id='authorizations_page_authorizations_title' />
+          </Typography>
+        }
+      >
+        <CardContent
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'flex-start',
+            gap: theme.spacing(4),
+          }}
+        >
+          <Typography
+            sx={{
+              fontSize: theme.utils.pxToRem(16),
+              fontStyle: 'normal',
+              fontWeight: theme.typography.fontWeightMedium,
+              lineHeight: theme.typography.lineHeight.lineHeight24px,
+              letterSpacing: '0.5px',
+              color: theme.palette.textSecondary,
+            }}
+          >
+            <Trans id='authorizations_page_description' />
+          </Typography>
+          <FormControlLabel
+            control={<Checkbox defaultChecked />}
+            label={<Trans id='authorizations_page_list_publications' />}
+          />
+          <FormControlLabel
+            control={<Checkbox defaultChecked />}
+            label={<Trans id='authorizations_page_my_expertise' />}
+          />
+        </CardContent>
+      </CustomCard>
+    </Box>
   )
 }
--- src/app/[lang]/myAccount/components/myProfile/index.tsx
+++ src/app/[lang]/myAccount/components/myProfile/index.tsx
@@ -1,25 +1,3 @@
-import Button from '@/app/theme/overrides/Button'
-import { CardActions, CardContent, Typography } from '@mui/material'
-
 export default function MyProfilePage() {
-  return (
-    <>
-      <CardContent>
-        <Typography gutterBottom sx={{ color: 'text.secondary', fontSize: 14 }}>
-          Word of the Day
-        </Typography>
-        <Typography variant='h5' component='div'>
-          lorem
-        </Typography>
-        <Typography sx={{ color: 'text.secondary', mb: 1.5 }}>
-          adjective
-        </Typography>
-        <Typography variant='body2'>
-          well meaning and kindly.
-          <br />
-          {'"a benevolent smile"'}
-        </Typography>
-      </CardContent>
-    </>
-  )
+  return <></>
 }
