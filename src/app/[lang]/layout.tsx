58037e0e5622343bec15c47c760e328a7ee24b62
KHADHRAOUI bilel <khadhraoui.bilel@devoskills.com>
2025-02-17 09:50:22 +0100

    fix icon

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -28,7 +28,7 @@ export default async function RootLayout({ params, children }: Props) {
       <head>
         <link
           rel='stylesheet'
-          href='https://fonts.googleapis.com/icon?family=Material+Icons'
+          href='https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&icon_names=podium'
         />
         <title>sovisuplus</title>
       </head>
