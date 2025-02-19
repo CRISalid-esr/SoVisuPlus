d4e55656f79828e67015620ca42b68a048117460
KHADHRAOUI bilel <khadhraoui.bilel@devoskills.com>
2025-02-19 12:02:33 +0100

    refacto PlatformChangesList

--- src/app/[lang]/documents/components/PlatformChangesList.tsx
+++ src/app/[lang]/documents/components/PlatformChangesList.tsx
@@ -4,23 +4,54 @@ import { List, ListItem, ListItemIcon, ListItemText } from '@mui/material'
 import { useTheme } from '@mui/material/styles'
 import { FC } from 'react'
 
-const PlatformChangesList: FC<{
-  changes: { added: number; updated: number; deleted: number }
-}> = ({ changes }) => {
+type ChangeType = 'added' | 'updated' | 'deleted'
+
+const PlatformChangesList: FC<{ changes: Record<ChangeType, number> }> = ({
+  changes,
+}) => {
   const theme = useTheme()
+
   return (
     <List>
-      {Object.entries(changes).map(([key, value]) => (
-        <ListItem key={key}>
+      {changes.added > 0 && (
+        <ListItem>
           <ListItemIcon>
             <FiberManualRecordIcon sx={{ fontSize: theme.utils.pxToRem(8) }} />
           </ListItemIcon>
           <ListItemText>
-            {value}{' '}
-            <Trans>{`documents_page_synchronize_modal_synchronize_success_tooltip_${key}_message`}</Trans>
+            {changes.added}{' '}
+            <Trans>
+              documents_page_synchronize_modal_synchronize_success_tooltip_added_message
+            </Trans>
           </ListItemText>
         </ListItem>
-      ))}
+      )}
+      {changes.updated > 0 && (
+        <ListItem>
+          <ListItemIcon>
+            <FiberManualRecordIcon sx={{ fontSize: theme.utils.pxToRem(8) }} />
+          </ListItemIcon>
+          <ListItemText>
+            {changes.updated}{' '}
+            <Trans>
+              documents_page_synchronize_modal_synchronize_success_tooltip_updated_message
+            </Trans>
+          </ListItemText>
+        </ListItem>
+      )}
+      {changes.deleted > 0 && (
+        <ListItem>
+          <ListItemIcon>
+            <FiberManualRecordIcon sx={{ fontSize: theme.utils.pxToRem(8) }} />
+          </ListItemIcon>
+          <ListItemText>
+            {changes.deleted}{' '}
+            <Trans>
+              documents_page_synchronize_modal_synchronize_success_tooltip_deleted_message
+            </Trans>
+          </ListItemText>
+        </ListItem>
+      )}
     </List>
   )
 }
