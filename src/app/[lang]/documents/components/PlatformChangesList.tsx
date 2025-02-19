eb01d59ae6fe9b61fc82231d8a875a82e2ef7021
KHADHRAOUI bilel <khadhraoui.bilel@devoskills.com>
2025-02-19 12:06:56 +0100

    add translate

--- src/app/[lang]/documents/components/PlatformChangesList.tsx
+++ src/app/[lang]/documents/components/PlatformChangesList.tsx
@@ -13,45 +13,90 @@ const PlatformChangesList: FC<{ changes: Record<ChangeType, number> }> = ({
 
   return (
     <List>
-      {changes.added > 0 && (
-        <ListItem>
-          <ListItemIcon>
-            <FiberManualRecordIcon sx={{ fontSize: theme.utils.pxToRem(8) }} />
-          </ListItemIcon>
-          <ListItemText>
-            {changes.added}{' '}
-            <Trans>
-              documents_page_synchronize_modal_synchronize_success_tooltip_added_message
-            </Trans>
-          </ListItemText>
-        </ListItem>
-      )}
-      {changes.updated > 0 && (
-        <ListItem>
-          <ListItemIcon>
-            <FiberManualRecordIcon sx={{ fontSize: theme.utils.pxToRem(8) }} />
-          </ListItemIcon>
-          <ListItemText>
-            {changes.updated}{' '}
-            <Trans>
-              documents_page_synchronize_modal_synchronize_success_tooltip_updated_message
-            </Trans>
-          </ListItemText>
-        </ListItem>
-      )}
-      {changes.deleted > 0 && (
-        <ListItem>
-          <ListItemIcon>
-            <FiberManualRecordIcon sx={{ fontSize: theme.utils.pxToRem(8) }} />
-          </ListItemIcon>
-          <ListItemText>
-            {changes.deleted}{' '}
-            <Trans>
-              documents_page_synchronize_modal_synchronize_success_tooltip_deleted_message
-            </Trans>
-          </ListItemText>
-        </ListItem>
-      )}
+      <ListItem>
+        <ListItemIcon
+          sx={{
+            marginRight: 0,
+          }}
+        >
+          <FiberManualRecordIcon
+            sx={{
+              fontSize: theme.utils.pxToRem(8),
+              color: theme.palette.white,
+            }}
+          />
+        </ListItemIcon>
+        <ListItemText
+          sx={{
+            fontWeight: theme.typography[500],
+            fontSize: theme.utils.pxToRem(16),
+            lineHeight: theme.typography.lineHeight.lineHeight16px,
+            fontStyle: 'normal',
+            letterSpacing: '0.5px',
+          }}
+        >
+          {changes.added || 0}{' '}
+          <Trans>
+            documents_page_synchronize_modal_synchronize_success_tooltip_added_message
+          </Trans>
+        </ListItemText>
+      </ListItem>
+      <ListItem>
+        <ListItemIcon
+          sx={{
+            marginRight: 0,
+          }}
+        >
+          <FiberManualRecordIcon
+            sx={{
+              fontSize: theme.utils.pxToRem(8),
+              color: theme.palette.white,
+            }}
+          />{' '}
+        </ListItemIcon>
+        <ListItemText
+          sx={{
+            fontWeight: theme.typography[500],
+            fontSize: theme.utils.pxToRem(16),
+            lineHeight: theme.typography.lineHeight.lineHeight16px,
+            fontStyle: 'normal',
+            letterSpacing: '0.5px',
+          }}
+        >
+          {changes.updated || 0}{' '}
+          <Trans>
+            documents_page_synchronize_modal_synchronize_success_tooltip_updated_message
+          </Trans>
+        </ListItemText>
+      </ListItem>
+      <ListItem>
+        <ListItemIcon
+          sx={{
+            marginRight: 0,
+          }}
+        >
+          <FiberManualRecordIcon
+            sx={{
+              fontSize: theme.utils.pxToRem(8),
+              color: theme.palette.white,
+            }}
+          />{' '}
+        </ListItemIcon>
+        <ListItemText
+          sx={{
+            fontWeight: theme.typography[500],
+            fontSize: theme.utils.pxToRem(16),
+            lineHeight: theme.typography.lineHeight.lineHeight16px,
+            fontStyle: 'normal',
+            letterSpacing: '0.5px',
+          }}
+        >
+          {changes.deleted || 0}{' '}
+          <Trans>
+            documents_page_synchronize_modal_synchronize_success_tooltip_deleted_message
+          </Trans>
+        </ListItemText>
+      </ListItem>
     </List>
   )
 }
