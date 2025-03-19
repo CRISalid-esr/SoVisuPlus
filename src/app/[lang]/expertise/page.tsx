eb406def92b1bc1ef8ce0d0b947b61f744a90c5d
jdp1ps <joachim.dornbusch@univ-paris1.fr>
2025-03-19 08:30:59 +0100

    Display perspectiive name on other pages

--- src/app/[lang]/expertise/page.tsx
+++ src/app/[lang]/expertise/page.tsx
@@ -1,14 +1,23 @@
 'use client'
 
-import { Trans } from '@lingui/macro'
-import { Box, Typography } from '@mui/material'
+import { t } from '@lingui/macro'
+import { Box } from '@mui/material'
+import { ExtendedLanguageCode } from '@/types/ExtendLanguageCode'
+import DocumentHeader from '@/app/[lang]/documents/components/DocumentHeader'
+import useStore from '@/stores/global_store'
+import * as Lingui from '@lingui/core'
 
 export default function ExpertisePage() {
+  const { currentPerspective } = useStore((state) => state.user)
+  const lang = Lingui.i18n.locale as ExtendedLanguageCode
   return (
-    <Box sx={{ padding: 4 }}>
-      <Typography variant='h4' gutterBottom>
-        <Trans>side_bar_expertise</Trans>
-      </Typography>
+    <Box>
+      <DocumentHeader
+        perspective={
+          currentPerspective?.getDisplayName(lang as ExtendedLanguageCode) || ''
+        }
+        pageName={t`expertise_page_main_title`}
+      />
     </Box>
   )
 }
