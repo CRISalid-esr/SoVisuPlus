68b93102db9ad455acb5bf9c1d2304a0bc7bb46f
jdp1ps <joachim.dornbusch@univ-paris1.fr>
2025-04-21 02:47:23 +0200

    Add test for login button

---
+++ src/app/[lang]/account/components/myProfile/components/identifiers/OrcidLoginButton.test.tsx
@@ -0,0 +1,60 @@
+import { render, screen } from '@testing-library/react'
+import { I18nProvider } from '@lingui/react'
+import { i18n } from '@lingui/core'
+import { OrcidLoginButton } from '@/app/[lang]/account/components/myProfile/components/identifiers/OrciLoginButton'
+
+process.env.NEXT_PUBLIC_ORCID_CLIENT_ID = 'client-id'
+process.env.NEXT_PUBLIC_ORCID_URL = 'https://orcid.org'
+process.env.NEXT_PUBLIC_ORCID_SCOPES = '/authenticate'
+process.env.NEXT_PUBLIC_SOVISUPLUS_HOST = 'https://example.com'
+
+i18n.load({
+  en: {
+    orcid_button_update_identifier: 'Update ORCID identifier',
+    orcid_button_provide_identifier: 'Provide ORCID identifier',
+  },
+})
+i18n.activate('en')
+
+const renderWithProviders = (props: { orcidProvided: boolean }) =>
+  render(
+    <I18nProvider i18n={i18n}>
+      <OrcidLoginButton {...props} />
+    </I18nProvider>,
+  )
+
+describe('OrcidLoginButton', () => {
+  it('displays "update identifier" text if ORCID is provided', () => {
+    renderWithProviders({ orcidProvided: true })
+
+    expect(
+      screen.getByRole('link', {
+        name: `ORCID logo ${i18n.t('orcid_button_update_identifier')}`,
+      }),
+    ).toBeInTheDocument()
+  })
+
+  it('displays "provide identifier" text if ORCID is not provided', () => {
+    renderWithProviders({ orcidProvided: false })
+
+    expect(
+      screen.getByRole('link', {
+        name: `ORCID logo ${i18n.t('orcid_button_provide_identifier')}`,
+      }),
+    ).toBeInTheDocument()
+  })
+
+  it('has correct href URL for ORCID authorization', () => {
+    renderWithProviders({ orcidProvided: false })
+
+    const expectedRedirect = encodeURIComponent(
+      'https://example.com/api/orcid/callback?lang=en',
+    )
+    const expectedScope = encodeURIComponent('/authenticate')
+
+    const expectedHref = `https://orcid.org/oauth/authorize?client_id=client-id&response_type=code&scope=${expectedScope}&redirect_uri=${expectedRedirect}`
+
+    const link = screen.getByRole('link')
+    expect(link).toHaveAttribute('href', expectedHref)
+  })
+})
