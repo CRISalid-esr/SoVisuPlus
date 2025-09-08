7a069ef202ab777be3069f478d45d774ffd8c4e2
Yanis Vieilly <yanis.vieilly@ehess.fr>
2025-09-08 16:18:55 +0200

    Merge migrations

--- prisma/migrations/20250908130647_update_journal_title_to_not_be_null/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Made the column `title` on table `Journal` required. This step will fail if there are existing NULL values in that column.
-
-*/
--- AlterTable
-ALTER TABLE "Journal" ALTER COLUMN "title" SET NOT NULL;
--- prisma/migrations/20250908141831_update_journal_titles_to_title/migration.sql
+++ prisma/migrations/20250908141831_update_journal_titles_to_title/migration.sql
@@ -2,8 +2,9 @@
   Warnings:
 
   - You are about to drop the column `titles` on the `Journal` table. All the data in the column will be lost.
+  - Added the required column `title` to the `Journal` table without a default value. This is not possible if the table is not empty.
 
 */
 -- AlterTable
 ALTER TABLE "Journal" DROP COLUMN "titles",
-ADD COLUMN     "title" TEXT;
+ADD COLUMN     "title" TEXT NOT NULL;
