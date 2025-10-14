893842b08032c2001501361244ccdbbd74bc8296
jdp1ps <joachim.dornbusch@univ-paris1.fr>
2025-10-14 08:31:40 +0200

    Fix t usage outside component lifecycle

--- src/app/[lang]/documents/[uid]/components/BibliographicInformation/Type.tsx
+++ src/app/[lang]/documents/[uid]/components/BibliographicInformation/Type.tsx
@@ -1,9 +1,13 @@
 import useStore from '@/stores/global_store'
 
 import { DocumentTypeLabels } from '../../../components/DocumentTypeLabels'
+import { useLingui } from '@lingui/react'
 
 export default function Type() {
   const { selectedDocument = null } = useStore((state) => state.document)
+  const { _ } = useLingui()
 
-  return selectedDocument && DocumentTypeLabels[selectedDocument.documentType]
+  return (
+    selectedDocument && _(DocumentTypeLabels[selectedDocument.documentType])
+  )
 }
--- src/app/[lang]/documents/components/DocumentTypeLabels.tsx
+++ src/app/[lang]/documents/components/DocumentTypeLabels.tsx
@@ -1,31 +1,15 @@
-import { Typography } from '@mui/material'
-import { t } from '@lingui/macro'
+import { defineMessage } from '@lingui/macro'
 
 import { DocumentType } from '@/types/Document'
+import { MessageDescriptor } from '@lingui/core'
 
-export const DocumentTypeLabels: Record<DocumentType, JSX.Element> = {
-  [DocumentType.Document]: (
-    <Typography>{t`documents_page_document_icon_label`}</Typography>
-  ),
-  [DocumentType.ScholarlyPublication]: (
-    <Typography>{t`documents_page_scholarly_publication_icon_label`}</Typography>
-  ),
-  [DocumentType.JournalArticle]: (
-    <Typography>{t`documents_page_journal_article_icon_label`}</Typography>
-  ),
-  [DocumentType.Book]: (
-    <Typography>{t`documents_page_book_icon_label`}</Typography>
-  ),
-  [DocumentType.Monograph]: (
-    <Typography>{t`documents_page_monograph_icon_label`}</Typography>
-  ),
-  [DocumentType.BookChapter]: (
-    <Typography>{t`documents_page_book_chapter_icon_label`}</Typography>
-  ),
-  [DocumentType.ConferenceArticle]: (
-    <Typography>{t`documents_page_conference_article_icon_label`}</Typography>
-  ),
-  [DocumentType.Proceedings]: (
-    <Typography>{t`documents_page_proceedings_icon_label`}</Typography>
-  ),
+export const DocumentTypeLabels: Record<DocumentType, MessageDescriptor> = {
+  [DocumentType.Document]: defineMessage`documents_page_document_icon_label`,
+  [DocumentType.ScholarlyPublication]: defineMessage`documents_page_scholarly_publication_icon_label`,
+  [DocumentType.JournalArticle]: defineMessage`documents_page_journal_article_icon_label`,
+  [DocumentType.Book]: defineMessage`documents_page_book_icon_label`,
+  [DocumentType.Monograph]: defineMessage`documents_page_monograph_icon_label`,
+  [DocumentType.BookChapter]: defineMessage`documents_page_book_chapter_icon_label`,
+  [DocumentType.ConferenceArticle]: defineMessage`documents_page_conference_article_icon_label`,
+  [DocumentType.Proceedings]: defineMessage`documents_page_proceedings_icon_label`,
 }
