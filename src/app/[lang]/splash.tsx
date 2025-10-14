207457a9035d5419ae59887a79c60345c44e3bb8
Yanis Vieilly <yanis.vieilly@ehess.fr>
2025-10-14 16:49:37 +0200

    Update left-side panel

--- src/app/[lang]/splash.tsx
+++ src/app/[lang]/splash.tsx
@@ -2,33 +2,92 @@
 
 import { Box, Button, Typography } from '@mui/material'
 import { t } from '@lingui/macro'
+import Image from 'next/image'
 import { signIn } from 'next-auth/react'
+import { useTheme } from '@mui/material/styles'
+import { Link } from '@mui/material'
 
 import Logo from '@/public/icons/logo_splash_screen.svg'
 import Background from '@/public/icons/background.svg'
+import CrisalidLogo from '@/public/icons/crisalid.png'
+import SplashPreview from '@/public/theme/splash_preview.png'
 
-export default function splash() {
+export default function Splash() {
+  const theme = useTheme()
+
   return (
     <>
-      <Box flex={{ xs: 1, md: 1 }}>
+      <Box flex={{ xs: 1, md: 1 }} display='flex' flexDirection='column'>
         <Box p={6}>
           <Logo />
         </Box>
 
-        <Box display='flex' justifyContent='center' alignItems='center'>
-          <Box width='100%' maxWidth='400px'>
-            <Button
-              onClick={() => signIn('keycloak')}
-              fullWidth
-              variant='contained'
-              sx={{
-                backgroundColor: 'teal',
-                mb: 2,
-                '&:hover': { backgroundColor: 'darkcyan' },
-              }}
-            >
-              {t`splash.login`}
-            </Button>
+        <Box
+          flex={1}
+          display='flex'
+          flexDirection='column'
+          justifyContent='center'
+          alignItems='center'
+          px={15}
+          pb={21}
+        >
+          <Image
+            src={SplashPreview}
+            alt='SoVisuPlus preview screenshot'
+            width={478}
+            height={236}
+            style={{
+              boxShadow: '0px 4px 4px 0px rgba(0, 0, 0, 0.25)',
+              marginBottom: '3.5rem',
+            }}
+          />
+
+          <Typography
+            component='p'
+            variant='headingSmall'
+            mb={7}
+            align='center'
+            color={theme.palette.primary.main}
+            fontWeight='bold'
+          >
+            Scientific Output Visualization
+          </Typography>
+
+          <Button
+            onClick={() => signIn('keycloak')}
+            fullWidth
+            variant='contained'
+            sx={{
+              maxWidth: '30rem',
+            }}
+          >
+            {t`splash.login`}
+          </Button>
+        </Box>
+
+        <Box pl={6} pr={3} py={2} display='flex' justifyContent='space-between'>
+          <Box>
+            <Image
+              src={CrisalidLogo}
+              alt='CRISalid logo'
+              width={150}
+              height={45}
+            />
+          </Box>
+
+          <Box display='flex' gap={3} alignItems='center' pb={1}>
+            <Link href='#' underline='none' fontWeight='bold'>
+              À propos
+            </Link>
+            <Link href='#' underline='none' fontWeight='bold'>
+              Mentions légales
+            </Link>
+            <Link href='#' underline='none' fontWeight='bold'>
+              Accessibilité
+            </Link>
+            <Link href='#' underline='none' fontWeight='bold'>
+              Communauté
+            </Link>
           </Box>
         </Box>
       </Box>
@@ -40,7 +99,7 @@ export default function splash() {
           alignItems='center'
           flexDirection='column'
           px={15}
-          py={24}
+          py={21}
           bgcolor='teal'
           color='white'
           position='relative'
@@ -58,8 +117,8 @@ export default function splash() {
             maxWidth='60ch'
             display='flex'
             flexDirection='column'
-            justifyContent='flex-end'
-            flex={1}
+            justifyContent='center'
+            flex={2}
           >
             <Typography component='h1' variant='displayLarge' mb={5}>
               Prenez le contrôle de l’empreinte numérique de vos recherches
