d60e072be9d942a40b987fd8b92d648de7033039
Yanis Vieilly <yanis.vieilly@ehess.fr>
2025-10-23 05:46:45 +0200

    398 finalize splash screen (#507)
    * Fix splash screen on mobile
    
    * Add localized custom text
    
    * Load splash screen preview dynamically
    
    * Move preview and footer outside of Next bundle
    
    * Move header logo, background, and locales outside of Next bundle

--- src/app/[lang]/splash.tsx
+++ src/app/[lang]/splash.tsx
@@ -1,107 +1,188 @@
 'use client'
 
+import { useEffect, useState } from 'react'
 import { Box, Button, Typography } from '@mui/material'
 import { t } from '@lingui/macro'
-import Logo from '@/public/icons/logo_splash_screen.svg'
-import Avatars from '@/public/icons/avatars.svg'
-import { signIn,  } from 'next-auth/react'
-import Background from '@/public/icons/background.svg'
+import { signIn } from 'next-auth/react'
+import { useTheme } from '@mui/material/styles'
+import { Link } from '@mui/material'
+import * as Lingui from '@lingui/core'
 
-export default function splash() {
+import { ThemeLocales } from '@/types/ThemeLocales'
+
+export default function Splash() {
+  const lang = Lingui.i18n.locale || 'fr'
+  const [locales, setLocales] = useState<ThemeLocales>()
+  const theme = useTheme()
+
+  useEffect(() => {
+    async function importLocales() {
+      try {
+        const response = await fetch(`/theme/locales_${lang}.json`)
+        const importedLocales = await response.json()
+        setLocales(importedLocales)
+      } catch {
+        throw new Error('Theme locales could not be imported')
+      }
+    }
+
+    importLocales()
+  }, [lang])
+
   return (
     <>
-      <Box flex={{ xs: 1, md: 1 }}>
+      <Box flex={1} display='flex'>
         <Box
-          sx={(theme) => ({
-            padding: theme.spacing(6),
-          })}
+          flex={1}
+          display='flex'
+          flexDirection='column'
+          px={2}
+          pt={{ xs: 2, md: 6 }}
+          pb={2}
         >
-          <Logo />
-        </Box>
-        <Box display='flex' justifyContent='center' alignItems='center'>
-          <Box width='100%' maxWidth='400px'>
+          <Box pl={{ lg: 3 }}>
+            {/* For all images on this page, <img> is used instead of <Image> so
+                that Next.js doesn't include it in the bundle, and instead
+                delivers it as is. It can then be replaced with a custom one at
+                runtime. */}
+            {/* eslint-disable-next-line @next/next/no-img-element */}
+            <img
+              src='/theme/splash_header_logo.svg'
+              alt='SoVisuPlus logo'
+              style={{ maxHeight: '4.125rem' }}
+            />
+          </Box>
+
+          <Box
+            flex={1}
+            display='flex'
+            flexDirection='column'
+            justifyContent='center'
+            alignItems='center'
+          >
+            <Box mb={7}>
+              {/* eslint-disable-next-line @next/next/no-img-element */}
+              <img
+                src='/theme/splash_preview.png'
+                alt='SoVisuPlus preview screenshot'
+                style={{
+                  boxShadow: '0px 4px 4px 0px rgba(0, 0, 0, 0.25)',
+                  height: 'auto',
+                  maxHeight: '14.75rem',
+                  maxWidth: '100%',
+                  width: '100%',
+                }}
+              />
+            </Box>
+
+            <Typography
+              component='p'
+              variant='headingSmall'
+              mb={7}
+              align='center'
+              color={theme.palette.primary.main}
+              fontWeight='bold'
+            >
+              {locales?.previewLegend}
+            </Typography>
+
             <Button
               onClick={() => signIn('keycloak')}
               fullWidth
               variant='contained'
               sx={{
-                backgroundColor: 'teal',
-                mb: 2,
-                '&:hover': { backgroundColor: 'darkcyan' },
+                maxWidth: '30rem',
               }}
             >
               {t`splash.login`}
             </Button>
           </Box>
+
+          <Box display='flex' justifyContent='space-between' pl={{ lg: 3 }}>
+            <Box
+              display='flex'
+              flexDirection={{ xs: 'column', lg: 'row' }}
+              justifyContent={{
+                xs: 'flex-end',
+                lg: 'flex-start',
+              }}
+            >
+              {/* eslint-disable-next-line @next/next/no-img-element */}
+              <img
+                src='/theme/splash_footer_logo.png'
+                alt='CRISalid logo'
+                style={{ maxHeight: '2.8125rem' }}
+              />
+            </Box>
+
+            <Box
+              display='flex'
+              gap={2.25}
+              alignItems={{ xs: 'flex-end', lg: 'center' }}
+              pb={{ xs: 2, md: 0 }}
+              flexDirection={{ xs: 'column', lg: 'row' }}
+              textAlign='right'
+            >
+              <Link href='#' underline='none' fontWeight='bold'>
+                {t`splash.about`}
+              </Link>
+              <Link href='#' underline='none' fontWeight='bold'>
+                {t`splash.legal_mentions`}
+              </Link>
+              <Link href='#' underline='none' fontWeight='bold'>
+                {t`splash.accessibility`}
+              </Link>
+              <Link href='#' underline='none' fontWeight='bold'>
+                {t`splash.community`}
+              </Link>
+            </Box>
+          </Box>
         </Box>
       </Box>
-      <Box
-        flex={1}
-        display={{ xs: 'none', md: 'flex' }} // Hide on small screens
-        justifyContent='center'
-        alignItems='center'
-        flexDirection='column'
-        p={4}
-        bgcolor='teal'
-        color='white'
-        position={'relative'}
-        sx={{
-          zIndex: 0,
-          overflow: 'hidden',
-        }}
-      >
-        <Background
-          style={{
-            position: 'absolute',
-            zIndex: -1,
-          }}
-        />
-        <Typography
-          variant='displayLarge'
-          sx={(theme) => ({
-            mb: theme.spacing(18),
-            zIndex: 1,
-          })}
-        >
-          Lorem Ipsum
-        </Typography>
+
+      <Box flex={1} display={{ xs: 'none', md: 'flex' }}>
         <Box
-          sx={{
-            maxWidth: '478.543px',
-            position: 'relative',
-          }}
+          display='flex'
+          flex={1}
+          alignItems='center'
+          justifyContent='center'
+          flexDirection='column'
+          px={{ md: 6, lg: 15 }}
+          py={{ md: 6, lg: 21 }}
+          bgcolor='teal'
+          color='white'
+          position='relative'
+          zIndex={0}
+          overflow='hidden'
         >
-          <Typography
-            component={'p'}
-            variant='headingSmall'
-            sx={(theme) => ({
-              mb: theme.spacing(18),
-              zIndex: 1,
-            })}
-          >
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua.
-          </Typography>
-        </Box>
-        <Box
-          sx={{
-            position: 'relative',
-            zIndex: 1,
-            display: 'flex',
-            justifyContent: 'space-between',
-            alignItems: 'center',
-          }}
-        >
-          <Avatars />
-          <Typography
-            variant='bodyLarge'
-            component={'p'}
-            sx={{
-              lineHeight: 'normal',
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <img
+            src='/theme/splash_background.svg'
+            alt='SoVisuPlus splash screen background'
+            style={{
+              position: 'absolute',
+              zIndex: -1,
             }}
+          />
+
+          <Box
+            maxWidth='60ch'
+            display='flex'
+            flexDirection='column'
+            justifyContent='center'
           >
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit
-          </Typography>
+            <Typography component='h1' variant='displayLarge' mb={5}>
+              {locales?.header}
+            </Typography>
+
+            <Typography component='p' variant='headingSmall' mb={3}>
+              {locales?.firstParagraph}
+            </Typography>
+
+            <Typography component='p' variant='headingSmall'>
+              {locales?.secondParagraph}
+            </Typography>
+          </Box>
         </Box>
       </Box>
     </>
