dd868daacb99f494b97374923e868b85786c4b2a
g-micha <Gabrielle.Micha@univ-paris1.fr>
2025-11-28 15:31:49 +0100

    add splash image and organisation logo customisation

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -2,12 +2,14 @@
 import { Box } from '@mui/material'
 import Splash from './splash'
 import UnauthenticatedRoute from '@/components/UnauthenticatedRoute'
+import { getSplashImages } from '@/utils/getSplashImages'
 
 export default async function Home() {
+  const splashImages = getSplashImages()
   return (
     <UnauthenticatedRoute>
       <Box display='flex' flexDirection='row' height='100vh' width='100vw'>
-        <Splash />
+        <Splash images={splashImages} />
       </Box>
     </UnauthenticatedRoute>
   )
