f6e2e88dae373975ac14ca737cbf349c072bd55c
jdp1ps <116366856+jdp1ps@users.noreply.github.com>
2026-01-24 12:01:00 +0100

    Don't use next cas client client side (#655)

--- src/app/[lang]/account/components/myProfile/components/identifiers/HalControl.tsx
+++ src/app/[lang]/account/components/myProfile/components/identifiers/HalControl.tsx
@@ -1,15 +1,43 @@
 import { Paper, Typography } from '@mui/material'
 import useStore from '@/stores/global_store'
 import { PersonIdentifierType } from '@/types/PersonIdentifier'
-import { login } from 'next-cas-client'
 import { getRuntimeEnv } from '@/utils/runtimeEnv'
 
+const redirectToCasLogin = ({
+  casUrl,
+  baseUrl,
+  renew,
+}: {
+  casUrl: string
+  baseUrl: string
+  renew?: boolean
+}) => {
+  if (!casUrl) throw new Error('Missing NEXT_PUBLIC_CAS_URL')
+  if (!baseUrl) throw new Error('Missing NEXT_PUBLIC_BASE_URL')
+
+  const normalizedCasUrl = casUrl.replace(/\/$/, '')
+  const normalizedBaseUrl = baseUrl.replace(/\/$/, '')
+
+  const serviceUrl = `${normalizedBaseUrl}/api/cas/login`
+
+  let loginUrl = `${normalizedCasUrl}/login?service=${encodeURIComponent(
+    serviceUrl,
+  )}`
+
+  if (renew) loginUrl += '&renew=true'
+
+  window.location.href = loginUrl
+}
+
 const HalControl = () => {
-  const sovisuplusHost = getRuntimeEnv().NEXT_PUBLIC_BASE_URL
-  const casUrl = getRuntimeEnv().NEXT_PUBLIC_CAS_URL
+  const env = getRuntimeEnv()
+  const sovisuplusHost = env.NEXT_PUBLIC_BASE_URL
+  const casUrl = env.NEXT_PUBLIC_CAS_URL
+
   const { connectedUser } = useStore((state) => state.user)
   const person = connectedUser?.person
   const identifiers = person?.getIdentifiers() ?? []
+
   const hal = identifiers.find(
     (identifier) => identifier.type === PersonIdentifierType.ID_HAL_S,
   )?.value
@@ -29,6 +57,7 @@ const HalControl = () => {
       <Typography variant='subtitle1' fontWeight='bold'>
         Hal
       </Typography>
+
       {hal ? (
         <Typography variant='body2'>{hal}</Typography>
       ) : (
@@ -36,8 +65,20 @@ const HalControl = () => {
           Not available
         </Typography>
       )}
-      <button onClick={() => login()}>Login</button>;
+
+      <button
+        onClick={() =>
+          redirectToCasLogin({
+            casUrl,
+            baseUrl: sovisuplusHost,
+            renew: false,
+          })
+        }
+      >
+        Login
+      </button>
     </Paper>
   )
 }
+
 export default HalControl
