fc0e22317c87495fa21ef49eecf33cab8943b8cc
g-micha <Gabrielle.Micha@univ-paris1.fr>
2026-03-04 10:46:12 +0100

    fix: remove static Vocab const vocabs and replace by getVocabs call and replace AVAILABLE_VOCABS injected variable name by the right one : NEXT_PUBLIC_AVAILABLE_VOCABS

--- src/app/types/Vocab.ts
+++ src/app/types/Vocab.ts
@@ -2,8 +2,6 @@ import { VOCABS } from '@/lib/services/Vocabs'
 import { getRuntimeEnv } from '@/utils/runtimeEnv'
 
 export class Vocab {
-  private static vocabs: string[] | null = null
-
   private readonly value: string
 
   private constructor(value: string) {
@@ -14,29 +12,22 @@ export class Vocab {
     return this.value
   }
 
-  private static init() {
-    if (this.vocabs === null) {
-      const env =
-        typeof window === 'undefined'
-          ? process.env.NEXT_PUBLIC_AVAILABLE_VOCABS
-          : getRuntimeEnv().NEXT_PUBLIC_AVAILABLE_VOCABS
-      this.vocabs = env?.split(',').map((v) => v.toLowerCase()) || []
-    }
-  }
-
   public static getVocabs() {
-    this.init()
-    return this.vocabs
+    const env =
+      typeof window === 'undefined'
+        ? process.env.NEXT_PUBLIC_AVAILABLE_VOCABS
+        : getRuntimeEnv().NEXT_PUBLIC_AVAILABLE_VOCABS
+    return env?.split(',').map((v) => v.toLowerCase()) || []
   }
 
   public static has(name: string) {
-    this.init()
-    const vocab = Vocab.vocabs?.find((vocab) => vocab === name.toLowerCase())
+    const vocab = Vocab.getVocabs()?.find(
+      (vocab) => vocab === name.toLowerCase(),
+    )
     return vocab !== undefined
   }
 
   public static fromString(name: string): Vocab {
-    this.init()
     const isVocab = Vocab.has(name.toLowerCase())
     if (!isVocab) {
       throw new Error(`${name} is not an available vocab`)
